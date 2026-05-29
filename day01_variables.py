c114f1b0e95d1886221c371ce2d80f3dea250fb1
Bhanu Maidhili <bhanumythili0909@gmail.com>
2026-05-29 22:50:19 +0530

    Update day01_variables.py

--- day01_variables.py
+++ day01_variables.py
@@ -1,13 +1,18 @@
 #Day 1: Understanding variables
 
 # Storing text(string)
+#  IP :  Internet protocol address (like physical street address but for computer on a network
 target_ip = "192.168.1.50"
 
 # Storing a whole number (integer)
+#  Port : ( like specific apartment number inside a building) 
+"""port 80 - unencrypted web traffic (HTTP)
+port 443 - for secure web traffic (HTTP)
+Port 22 - reserved for SSH (Secure Shell)"""
 port = 22
 
 # Storing a true/false value (Boolean)
-is_aactive = True
+is_active = True
 
 # Printing the variables to the screen
 print("Scanning IP:", target_ip)
