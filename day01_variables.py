a291c58721bce3ecae0426ea97a3038a2694f30c
Bhanu Maidhili <bhanumythili0909@gmail.com>
2026-05-29 22:29:31 +0530

    Day 01: defined basic python variables

---
+++ day01_variables.py
@@ -0,0 +1,15 @@
+#Day 1: Understanding variables
+
+# Storing text(string)
+target_ip = "192.168.1.50"
+
+# Storing a whole number (integer)
+port = 22
+
+# Storing a true/false value (Boolean)
+is_aactive = True
+
+# Printing the variables to the screen
+print("Scanning IP:", target_ip)
+print("Port Number:", port)
+print("System Active:", is_active)
